a35092959f5fff8cd0d1d7ed7678037b036202cf
vird <virdvip@gmail.com>
2019-10-15 00:42:10 +0200

    reorder prerequirements; add test faucet call

---
+++ docker_test/docker_4_send_wallet_boc.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# source ~/.bashrc not works
+docker exec -t -i `cat ../DOCKER_ID` /bin/bash -c "export FIFTPATH=~/lite-client/crypto/fift/lib && cd ~/dich4/prerequirement_scripts && ./4_send_wallet_boc.sh"
+
